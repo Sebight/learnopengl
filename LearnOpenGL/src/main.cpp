6e52b5277ff3a5fe6349c799cdb114e4ba74b0d3
Sebight <seb1rblx@gmail.com>
2024-11-02 17:15:59 +0100

    feat: EBO impl

--- LearnOpenGL/src/main.cpp
+++ LearnOpenGL/src/main.cpp
@@ -16,17 +16,44 @@ void processInput(GLFWwindow* window) {
 }
 
 float vertices[] = {
-	-0.5f, -0.5f, 0.0f,
-	0.0f, 0.5f, 0.0f,
-	0.5f, -0.5f, 0.0f,
+	-1.0f, 0.0f, 0.0f,  // Left
+	-0.5f, 0.5f, 0.0f,  // Top
+	0.0f, 0.0f, 0.0f,  // Right
+	0.5f, 0.5f, 0.0f,   // Top 2
+	1.0f, 0.0f, 0.0f // Right 2
 };
+
+float otherTri[] = {
+	-1.0f, -1.0f, 0.0f,
+	0.0f, -0.5f, 0.0f,
+	1.0f, -1.0f, 0.0f
+};
+
+unsigned int indices[] = {  // note that we start from 0!
+	0, 1, 2,   // first triangle
+	2, 3, 4    // second triangle
+};
+
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(1.0f, 1.0f, 0.0f, 1.0f);\n"
+"}\0";
 
 int main() {
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	
+
 	GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL", NULL, NULL);
 	if (!window) {
 		printf("Failed to create GLFW window.\n");
@@ -58,15 +85,12 @@ int main() {
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
+	uint EBO;
+	glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	// Vertex shader
-	const char* vertexShaderSource = "#version 330 core\n"
-		"layout (location = 0) in vec3 aPos;\n"
-		"void main()\n"
-		"{\n"
-		"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-		"}\0";
-
 	uint vertexShader;
 	vertexShader = glCreateShader(GL_VERTEX_SHADER);
 
@@ -83,13 +107,6 @@ int main() {
 	}
 
 	// Fragment shader
-	const char* fragmentShaderSource = "#version 330 core\n"
-		"out vec4 FragColor;\n"
-		"void main()\n"
-		"{\n"
-		"   FragColor = vec4(1.0f, 0.0f, 0.0f, 1.0f);\n"
-		"}\0";
-	
 	uint fragmentShader;
 	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 
@@ -106,11 +123,11 @@ int main() {
 	// Shader program
 	uint shaderProgram;
 	shaderProgram = glCreateProgram();
-	
+
 	glAttachShader(shaderProgram, vertexShader);
 	glAttachShader(shaderProgram, fragmentShader);
 	glLinkProgram(shaderProgram);
-	
+
 	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
 	if (!success) {
 		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
@@ -121,6 +138,7 @@ int main() {
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
 	while (!glfwWindowShouldClose(window)) {
 		// Input
@@ -132,7 +150,8 @@ int main() {
 
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
 
 
 		// Screen render
